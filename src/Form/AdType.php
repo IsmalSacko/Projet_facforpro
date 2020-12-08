df1d67c5f9b6b303945754eae0a42bd1e209df19
ISMAILA SACKO <ismalsacko@yahoo.fr>
2020-12-08 17:50:47 +0100

    Mise à jour des contenus de certaines pages

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -18,15 +19,31 @@ class AdType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('price', IntegerType::class)
+            ->add('title', TextType::class,[
+                'label'=>'Titre',
+                'attr' =>[
+                    'placeholder' => 'Le Titre ici'
+                ]
+            ])
+            ->add('price', IntegerType::class,[
+                'label'=>'Prix',
+                'attr' =>[
+                    'placeholder' => 'Uniqument un entier (1,2,3...)'
+                ]
+            ])
             ->add('content', TextareaType::class)
             ->add('imageFile', FileType::class, [
                 'data_class' => null,
+                'label'=>'Images',
                 'required' => false,
                 'allow_extra_fields' => true
             ])
-            ->add('room')
+            ->add('room',IntegerType::class,[
+                'label'=>'Quantité',
+                'attr' =>[
+                    'placeholder' => 'Quantité(entier)'
+                ]
+            ])
             ->add('utilisateur')
             // ->add('submit', SubmitType::class)
 
